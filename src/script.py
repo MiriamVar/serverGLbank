1843f36ee2528fe862cf8c46410e92dd17111360
Miriam Vargockova <mimika648@gmail.com>
2019-05-12 14:24:34 +0200

    one bug solved

--- src/script.py
+++ src/script.py
@@ -4,7 +4,7 @@ import mysql.connector
 app = Flask(__name__)  # spravi z tohto suboru web aplikaciu
 
 
-db = mysql.connector.connect(host='localhost', port='3308', database='glbank', user='root', password='')
+db = mysql.connector.connect(host='itsovy.sk', port='3306', database='glbank', user='glbank', password='password')
 
 
 @app.route("/")  # pripajanie na web
@@ -36,38 +36,47 @@ def register():
         queryBlockIB = "select * from loginhistory where idl = %s order by UNIX_TIMESTAMP(logDate) desc limit 3;"
         cur2.execute(queryBlockIB, (idClient,))
         records = cur2.fetchall()
-        success = []
-        for row in records:
-            print(row)
-            print(row[2])
-            success.append(row[2])
-
-        # ak posledny zaznam nie je null
-        if success[0] is not None:
-            # success ci sa == 1 alebo sa da zretazit ??
-            if success[0] == 1 and success[1] == 1 and success[2] == 1:
-                return "Your IB is blocked"
-            else:
-                # "Your IB is unblocked"
-                # overuje sa ucet
-                cur = db.cursor()
-                queryClient = "SELECT * FROM Client inner join loginclient on Client.id=loginclient.idc where login = %s and password = %s;"
-                cur.execute(queryClient, (login, password))
-                user = cur.fetchone()
-                print(user)
-                if user is None:
-                    # treba vlozit zapis do db
-                    cur4 = db.cursor()
-                    queryWrongLP = "insert into loginhistory (idl,success) values (%s,%s)"
-                    insert = cur4.execute(queryWrongLP, (idClient, 0))
-                    print(insert)
-                    db.commit()
-                    return "You typed wrong password"
-                else:
-                    return render_template("succesful.html")
-
+        print(records)
+        if not records:
+            cur4 = db.cursor()
+            queryWrongLP = "insert into loginhistory (idl,success) values (%s,%s)"
+            insert = cur4.execute(queryWrongLP, (idClient, 1))
+            print(insert)
+            db.commit()
+            return render_template("succesful.html")
         else:
-            return "Your IB ib blocked by Bank Employee"
+            success = []
+            for row in records:
+                print(row)
+                print(row[2])
+                success.append(row[2])
+
+            # ak posledny zaznam nie je null
+            if success[0] is not None:
+                # success ci sa == 1 alebo sa da zretazit ??
+                if success[0] == 1 and success[1] == 1 and success[2] == 1:
+                    return "Your IB is blocked"
+                else:
+                    # "Your IB is unblocked"
+                    # overuje sa ucet
+                    cur = db.cursor()
+                    queryClient = "SELECT * FROM Client inner join loginclient on Client.id=loginclient.idc where login = %s and password = %s;"
+                    cur.execute(queryClient, (login, password))
+                    user = cur.fetchone()
+                    print(user)
+                    if user is None:
+                        # treba vlozit zapis do db
+                        cur4 = db.cursor()
+                        queryWrongLP = "insert into loginhistory (idl,success) values (%s,%s)"
+                        insert = cur4.execute(queryWrongLP, (idClient, 0))
+                        print(insert)
+                        db.commit()
+                        return "You typed wrong password"
+                    else:
+                        return render_template("succesful.html")
+
+            else:
+                return "Your IB ib blocked by Bank Employee"
 
     db.close()
 
